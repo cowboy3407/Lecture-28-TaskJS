3f00c570e3a5a05560c346eb362642e5179731ed
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2024-12-23 19:27:03 +0400

    Finished

--- script.js
+++ script.js
@@ -14,11 +14,46 @@ function countdown() {
     const minuteNumber = Math.floor((difference % hour) / minute);
     const secondsNumber = Math.floor((difference % minute) / second);
 
-    document.querySelector('.days').textContent = dayNumber.toString().padStart(2, '0');
-    document.querySelector('.hours').textContent = hourNumber.toString().padStart(2, '0');
-    document.querySelector('.minutes').textContent = minuteNumber.toString().padStart(2, '0');
-    document.querySelector('.seconds').textContent = secondsNumber.toString().padStart(2, '0');
+    document.querySelector('.days').textContent = `0${dayNumber}`.slice(-2);
+    document.querySelector('.hours').textContent = `0${hourNumber}`.slice(-2);
+    document.querySelector('.minutes').textContent = `0${minuteNumber}`.slice(-2);
+    document.querySelector('.seconds').textContent = `0${secondsNumber}`.slice(-2);    
 }
 
 setInterval(countdown, 1000);
 countdown();
+
+
+const facebookIcon = document.querySelector('#facebook');
+const pinterestIcon = document.querySelector('#pinterest');
+const instagramIcon = document.querySelector('#instagram');
+
+facebookIcon.addEventListener('mouseover', () => {
+    facebookIcon.querySelector('svg').style.display = 'none';
+    facebookIcon.querySelector('img').style.display = 'inline-block';
+});
+
+facebookIcon.addEventListener('mouseout', () => {
+    facebookIcon.querySelector('img').style.display = 'none';
+    facebookIcon.querySelector('svg').style.display = 'inline-block';
+});
+
+pinterestIcon.addEventListener('mouseover', () => {
+    pinterestIcon.querySelector('svg').style.display = 'none';
+    pinterestIcon.querySelector('img').style.display = 'inline-block';
+});
+
+pinterestIcon.addEventListener('mouseout', () => {
+    pinterestIcon.querySelector('img').style.display = 'none';
+    pinterestIcon.querySelector('svg').style.display = 'inline-block';
+});
+
+instagramIcon.addEventListener('mouseover', () => {
+    instagramIcon.querySelector('svg').style.display = 'none';
+    instagramIcon.querySelector('img').style.display = 'inline-block';
+});
+
+instagramIcon.addEventListener('mouseout', () => {
+    instagramIcon.querySelector('img').style.display = 'none';
+    instagramIcon.querySelector('svg').style.display = 'inline-block';
+});
